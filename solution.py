c030c6f27e3c5b6cf6505c08a1c79214f67c41fb
jayraj <jayrajp846@gmail.com>
2021-10-31 10:21:18 +0100

    Restructured and documented code

--- solution.py
+++ solution.py
@@ -1,50 +1,56 @@
 import os
-from imaging_interview import preprocess_image, compare_frames
+from imaging_interview import preprocess_image_change_detection, compare_frames_change_detection
 import cv2
 import glob
-import time
 from tqdm import tqdm
-
+import argparse
 
 
 def is_similar(prev_frame, next_frame):
-    """
+    """Check similarity between two images
 
-    :param prev_frame:
-    :param next_frame:
-    :return:
+    :param  prev_frame: Previous frame path
+    :type prev_frame: str
+    :param next_frame: Next frame path
+    :type next_frame: str
+    :return: True for similar images
+    :rtype: bool
     """
     img1 = cv2.imread(prev_frame)
     img2 = cv2.imread(next_frame)
-    processed_img1 = preprocess_image(img1, [21])
-    processed_img2 = preprocess_image(img2, [21])
-    score, _, _ = compare_frames(processed_img1, processed_img2, 7000.0)
+    processed_img1 = preprocess_image_change_detection(img1, [21])
+    processed_img2 = preprocess_image_change_detection(img2, [21])
+    min_contour_area = 7000
+    score, _, _ = compare_frames_change_detection(processed_img1, processed_img2, min_contour_area)
     if score == 0:
         return True
     else:
         return False
 
 
-def remove_similar_images():
-    """
+def delete_similar_images(path):
+    """ Delete similar images from directory
 
-    :return:
+    :param path: Image directory path
+    :type path: str
+    :return: total deleted images
+    :rtype: int
     """
-    removed_image_list = []
+    deleted_image_list = []
     # remove similar consecutive image frames
-    image_frame_list = [img for img in glob.glob("c23/*.png")]
-    frame_pbar = tqdm(len(image_frame_list)-1)
+    image_frame_list = [img for img in glob.glob(path + "/*.png")]
+    frame_pbar = tqdm(len(image_frame_list) - 1)
     frame_pbar.set_description("Deleting consecutive similar image frames: ")
 
     for i in range(len(image_frame_list) - 1):
         frame_pbar.update()
         if is_similar(image_frame_list[i], image_frame_list[i + 1]):
-            removed_image_list.append(image_frame_list[i])
+            deleted_image_list.append(image_frame_list[i])
             os.remove(image_frame_list[i])
     frame_pbar.close()
 
     # remove the remaining similar images
-    image_list = [img for img in glob.glob("c23/*.png")]
+    image_list = [img for img in glob.glob(path + "/*.png")]
     i = 0
     img_pbar = tqdm(len(image_list))
     img_pbar.set_description("Deleting similar images: ")
@@ -55,15 +61,23 @@ def remove_similar_images():
         while j < len(image_list):
             if is_similar(image_list[i], image_list[j]):
                 os.remove(image_list[j])
-                removed_image_list.append(image_list[j])
+                deleted_image_list.append(image_list[j])
                 del image_list[j]
             else:
                 j += 1
         i += 1
     img_pbar.close()
 
-    return len(removed_image_list)
+    return len(deleted_image_list)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", help="image directory path to delete similar images")
+    args = parser.parse_args()
+    path = args.path
+    print(f"Total deleted images: {delete_similar_images(path)}")
 
 
 if __name__ == "__main__":
-    print(f"Total deleted images: {remove_similar_images()}")
+    main()
